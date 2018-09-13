7e13eeed898953b6470c98eb72f68d5cdba1b69a
Gabi Almeida <gabicfa1996@gmail.com>
2018-09-12 21:13:26 -0300

    Projeto finalizado

---
+++ Aula12/exemplo_fs/build/CMakeFiles/exemplo_fs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exemplo_fs.dir/exemplo_fs.cpp.o"
+  "exemplo_fs.pdb"
+  "exemplo_fs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exemplo_fs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Aula12/exemplo_fs/build/CMakeFiles/teste_omp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teste_omp.dir/teste_omp.cpp.o"
+  "teste_omp.pdb"
+  "teste_omp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teste_omp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
